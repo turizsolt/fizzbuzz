66873bf254b47d4e4f3b5c4808c5af370d14e053
turizsolt <turi.zsolt.m@gmail.com>
2017-07-31 21:00:10 +0200

    Added main.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
 /**
  * Created by zsiri on 2017.07.31..
  */
+
+import numberToFizzBuzz from './fizzbuzz';
+
+const MAX = 100;
+
+for(var i=0;i<MAX;i++) {
+    console.log(numberToFizzBuzz(i));
+}
