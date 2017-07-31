54012e4c831f917e3b8d9052258115a2a2490f0b
turizsolt <turi.zsolt.m@gmail.com>
2017-07-31 21:05:43 +0200

    Added consts to the FizzBuzz function.

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -2,9 +2,12 @@
  * Created by zsiri on 2017.07.31..
  */
 
+const FizzDivider = 3;
+const BuzzDivider = 5;
+
 export default function numberToFizzBuzz(n: number):string{
-    const isFizz = n%3 == 0;
-    const isBuzz = n%5 == 0;
+    const isFizz = n%FizzDivider == 0;
+    const isBuzz = n%BuzzDivider == 0;
 
     if(isFizz && isBuzz) return "FizzBuzz";
     if(isFizz) return "Fizz";
